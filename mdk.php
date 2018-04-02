a850b78ce773ef9a368e4051402bfc5690d7261e
Ashish Bishnoi <ashishbishnoi18@gmail.com>
2018-04-02 18:25:48 +0000

    Add files via upload

--- mdk.php
+++ mdk.php
@@ -2,9 +2,9 @@
 $card = shell_exec('iw dev | awk \'$1=="Interface"{print $2}\' | sed -e \'2d\' ');
 $message = "Monitor mode not  enabled";
 $moncheck = shell_exec('iwconfig wlan1 | grep Monitor | sed -e "s/Mode:/ /" | awk \'{print $1}\' | cut -b 5');
-$wps = shell_exec('sudo timeout 10 wash -i wlan1');
+$dall = shell_exec('sudo mdk3 wlan1 d ');
 	if ($moncheck == 1) {
-		echo "<pre>$wps</pre>";
+		echo "<pre>$dall</pre>";
 	}
 	else {
 	echo "<script type='text/javascript'>alert('$message');</script>";
